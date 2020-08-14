44a9a223b85aa1fd8471c883ea7377be8e321054
kosamson <kosamson1@gmail.com>
2020-08-14 15:41:26 -0700

    Add custom bot client class (convert to OOP)

--- src/CringBotApp.py
+++ src/CringBotApp.py
@@ -4,32 +4,31 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
+    
+# Personal token and guild
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+class CringBotApp(discord.Client):
+    # Command Prefix for CringBot is '+'; 
+    # Ex: "+help" -- Runs the "help" command from CringBot
+    COMMAND_PREFIX = commands.Bot(command_prefix = '+')
 
-# Command Prefix for CringBot is '+'; 
-# Ex: "+help" -- Runs the "help" command from CringBot
-COMMAND_PREFIX = commands.Bot(command_prefix = '+')
+    async def on_ready(self):
+        for guild in client.guilds:
+            if guild.name == GUILD:
+                break
+        
+        # Startup Checks
+        print(f'{client.user} is now connected.')
+        print(f'Connected to Guild: {guild.name}, Guild ID: {guild.id}')
+        
+        mainChannelID = int(input('Which text channel would you like this bot to be active? (Enter Channel ID): '))
+        mainChannel = client.get_channel(mainChannelID)
+        await mainChannel.send('TEST MESSAGE')
+        print('Message successfully sent.')
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-    
-    # Startup Checks
-    print(f'{client.user} is now connected.')
-    print(f'Connected to Guild: {guild.name}, Guild ID: {guild.id}')
-    
-    mainChannelID = int(input('Which text channel would you like this bot to be active? (Enter Channel ID): '))
-    mainChannel = client.get_channel(mainChannelID)
-    await mainChannel.send('TEST MESSAGE')
-    print('Message successfully sent.')
-
-
-# Run bot using token
+client = CringBotApp()
 client.run(TOKEN)
 
 
