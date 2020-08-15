a61528dabb88bf63dfe2496fff319c80948f5a07
kosamson <kosamson1@gmail.com>
2020-08-14 17:16:35 -0700

    Add cring response and +cring command

--- src/CringBotApp.py
+++ src/CringBotApp.py
@@ -12,7 +12,7 @@ GUILD = os.getenv('DISCORD_GUILD')
 class CringBotApp(discord.Client):
     # Command Prefix for CringBot is '+'; 
     # Ex: "+help" -- Runs the "help" command from CringBot
-    COMMAND_PREFIX = commands.Bot(command_prefix = '+')
+    COMMAND_PREFIX = '+'
 
     async def on_ready(self):
         for guild in client.guilds:
@@ -22,14 +22,31 @@ class CringBotApp(discord.Client):
         # Startup Checks
         print(f'{client.user} is now connected.')
         print(f'Connected to Guild: {guild.name}, Guild ID: {guild.id}')
-        
-        mainChannelID = int(input('Which text channel would you like this bot to be active? (Enter Channel ID): '))
-        mainChannel = client.get_channel(mainChannelID)
-        await mainChannel.send('TEST MESSAGE')
-        print('Message successfully sent.')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if (self.COMMAND_PREFIX in message.content):
+            await self.parseCommand(message)
+
+        # Check for cring
+        else:
+            await self.on_cring(message)
+
+    async def parseCommand(self, message):
+        if (message.content.lower())[1:] == 'cring':
+            await message.add_reaction('🇨')
+            await message.add_reaction('🇷')
+            await message.add_reaction('🇮')
+            await message.add_reaction('🇳')
+            await message.add_reaction('🇬')
+
+    
+    async def on_cring(self, message):
+        if 'cring' in message.content.lower():
+            await message.channel.send('CRING')
+
 
 client = CringBotApp()
 client.run(TOKEN)
-
-
-
